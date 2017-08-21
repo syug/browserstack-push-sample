872d0a6c585b8c93ffe3dc18999b3a87dfccd298
Saito, Shugo | Shugo | COED <shugo.saito@rakuten.com>
2017-08-21 14:32:13 +0900

    Added os settings

--- tests/sample.spec.js
+++ tests/sample.spec.js
@@ -12,12 +12,17 @@ chrome.setDefaultService(new chrome.ServiceBuilder(path).build());
 
 // Input capabilities
 var capabilities = Object.assign(conf, {
+  // https://www.browserstack.com/automate/node
   // Browser settings
   'browserName': 'chrome',
   'browser_version': '60.0', // If browser_version capability is not set, the test will run on the latest version of the browser set by browser capability.
   'os': 'OS X',
-  'os_version': 'Sierra',
-  'resolution': '1024x768',
+  // 'os_version': 'Sierra',
+  'os_version': 'El Capitan',
+  // 'os': 'Windows', // We can check the notification popup on Remote when the os is Windows
+  // 'os_version': '10',
+  // 'resolution': '1024x768',
+  'resolution': '1920x1080',
   'chromeOptions': { // chromeOptions can be set here
     'prefs': {
       'profile': {
@@ -53,6 +58,6 @@ var driver = new webdriver.Builder()
 driver.get('http://dev.izm.org.uk/push_notifications/');
 driver.sleep(1000);
 driver.findElement(webdriver.By.className('mdl-js-button')).click();
-driver.sleep(3000);
+driver.sleep(4000);
 
 driver.quit();
