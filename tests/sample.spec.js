18f0b8484345f7c1c35141d80b52c14a1cd06868
Saito, Shugo | Shugo | COED <shugo.saito@rakuten.com>
2017-08-22 09:41:42 +0900

    updated spec file

--- tests/sample.spec.js
+++ tests/sample.spec.js
@@ -17,12 +17,12 @@ var capabilities = Object.assign(conf, {
   'browserName': 'chrome',
   'browser_version': '60.0', // If browser_version capability is not set, the test will run on the latest version of the browser set by browser capability.
   'os': 'OS X',
-  // 'os_version': 'Sierra',
-  'os_version': 'El Capitan',
+  'os_version': 'Sierra',
+  // 'os_version': 'El Capitan',
   // 'os': 'Windows', // We can check the notification popup on Remote when the os is Windows
   // 'os_version': '10',
   // 'resolution': '1024x768',
-  'resolution': '1920x1080',
+  // 'resolution': '1920x1080',
   'chromeOptions': { // chromeOptions can be set here
     'prefs': {
       'profile': {
@@ -48,7 +48,7 @@ chromeOptions.setUserPreferences({
 });
 
 var driver = new webdriver.Builder()
-  // .usingServer('http://hub-cloud.browserstack.com/wd/hub') // for browserstack remote test!
+  .usingServer('http://hub-cloud.browserstack.com/wd/hub') // for browserstack remote test!
   .forBrowser('chrome')
   // .setChromeOptions(chromeOptions)  // chromeOptions can also be set here
   .withCapabilities(capabilities)
@@ -56,8 +56,8 @@ var driver = new webdriver.Builder()
 
 // connect to public address
 driver.get('http://dev.izm.org.uk/push_notifications/');
-driver.sleep(1000);
+driver.sleep(500);
 driver.findElement(webdriver.By.className('mdl-js-button')).click();
-driver.sleep(4000);
+driver.sleep(1000);
 
 driver.quit();
